0279889c774eb5affef9f0542e6b4238832dad66
viditsuri5 <viditsuri5@gmail.com>
2025-03-27 20:02:52 +0000

    new changes

--- TowerModel.java
+++ TowerModel.java
@@ -1,88 +1,43 @@
+import java.util.Stack;
+import java.util.List;
+import java.util.ArrayList;
+
 public class TowerModel {
+    private final int totalDisks;
+    private final List<Stack<Integer>> rods;
 
-    // 2d array storing game state
-    private IntegerStack[] towers;
-
-    // Final tower height
-    private int towerHeight = 0;
-
-    // Debug metrics
-    private int printCounter = 0;
-    private int moveCounter = 0;
-
-    /* This class implements a model of a tower of Hanoi game.
-
-        |    |    |
-        =    |    |
-       ===   |    |
-      =====  |    |
-     ------------------
-        0    1    2
-
-     Example of a game of height three in the starting position.
-
-    */
-    public TowerModel(int height)
-    {
-        towerHeight = height;
-        towers = new IntegerStack[3];
-        towers[0] = new IntegerStack(height);
-        towers[1] = new IntegerStack(height);
-        towers[2] = new IntegerStack(height);
-
-        for (int i=0; i<height; i++)
-        {
-            towers[0].push(height - i);
+    public TowerModel(int totalDisks) {
+        this.totalDisks = totalDisks;
+        rods = new ArrayList<>();
+        
+        // Initialize rods
+        for (int i = 0; i < 3; i++) {
+            rods.add(new Stack<>());
         }
-    }
 
-    // get the total number of disks
-    public int height()
-    {
-        return towerHeight;
-    }
-
-
-    // Move one disk from the source stack to the destination stack.
-    public void move(int source, int destination)
-    {
-        System.out.println("Move #" + ++moveCounter + " from " + source + " to " + destination);
-        // TODO!!
-    }
-
-    // Helper method to nicely print the current model state.
-    public void print()
-    {
-        System.out.print("Print #" + ++printCounter + " Towers of Hanoi\n");
-
-        for (int layer = towerHeight-1; layer >= 0; layer--){
-            System.out.print("\n");
-            for (int tower = 0; tower < towers.length; tower++) {
-                int value = towers[tower].get(layer);
-                if (value == 0)
-                {
-                    System.out.print(" |");
-                } else
-                {
-                    System.out.print(" " + value);
-                }
+        for (int i = totalDisks; i > 0; i--) {
+            rods.get(0).push(i);
             }
         }
-        System.out.println();
-        for (int i = 0; i < towers.length; i++) {
-            System.out.print(" =");
+
+        public void move(int fromRod, int toRod) {
+        if (rods.get(fromRod).isEmpty()) {
+            throw new IllegalStateException("Cannot move from an empty rod!");
+        }
+        if (!rods.get(toRod).isEmpty() && rods.get(fromRod).peek() > rods.get(toRod).peek()) {
+            throw new IllegalStateException("Cannot place a larger disk on a smaller one!");
+        }
+        rods.get(toRod).push(rods.get(fromRod).pop());
+    }
+
+    public boolean isSolved() {
+        return rods.get(0).isEmpty() && rods.get(1).isEmpty() && rods.get(2).size() == totalDisks;
+    }
+
+    public void print() {
+        for (int i = 0; i < rods.size(); i++) {
+            System.out.println("Rod " + i + ": " + rods.get(i));
         }
         System.out.println();
-    }
-
-    // Test instrumentation
-    public IntegerStack[] getTowers()
-    {
-        return towers;
-    }
-
-    public int getHeight()
-    {
-        return towerHeight;
     }
 }
